2f4cb8c69047773c30fd3760bc5ffa16b3af52f4
MichaelMathew <2072007@maranatha.ac.id>
2023-01-01 17:29:40 +0700

    menambahkan button logout
    menambahkan button logout

--- app/src/main/java/com/michael/tubesmobile/SecondActivity.kt
+++ app/src/main/java/com/michael/tubesmobile/SecondActivity.kt
@@ -1,11 +1,29 @@
 package com.michael.tubesmobile
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.michael.tubesmobile.databinding.ActivityMainBinding
+import com.michael.tubesmobile.databinding.ActivitySecondBinding
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySecondBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        firebaseAuth = Firebase.auth
+        binding.btnLogout.setOnClickListener {
+            Firebase.auth.signOut()
+            val intent = Intent(
+                this,
+                MainActivity::class.java
+            )
+            startActivity(intent)
+        }
     }
 }
