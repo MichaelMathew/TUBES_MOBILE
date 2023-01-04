e56122fe5ace29a24dee7a3fef51169a32da25b9
Arya Majiah <71741042+aryatripm@users.noreply.github.com>
2023-01-04 20:35:52 +0700

    Viewmodel

--- app/src/main/java/com/michael/tubesmobile/SecondActivity.kt
+++ app/src/main/java/com/michael/tubesmobile/SecondActivity.kt
@@ -3,6 +3,7 @@ package com.michael.tubesmobile
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -12,6 +13,7 @@ import com.michael.tubesmobile.databinding.ActivitySecondBinding
 class SecondActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySecondBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondBinding.inflate(layoutInflater)
@@ -25,5 +27,26 @@ class SecondActivity : AppCompatActivity() {
             )
             startActivity(intent)
         }
+        binding.btnCalculate.setOnClickListener {
+            val width = binding.edtWidth.text.toString()
+            val height = binding.edtHeight.text.toString()
+            val length = binding.edtLength.text.toString()
+            when {
+                width.isEmpty() -> {
+                    binding.edtWidth.error = "Masih kosong"
+                }
+                height.isEmpty() -> {
+                    binding.edtHeight.error = "Masih kosong"
+                }
+                length.isEmpty() -> {
+                    binding.edtLength.error = "Masih kosong"
+                }
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    binding.tvResult.text = viewModel.result.toString()
+                }
+            }
+        }
+        binding.tvResult.text = viewModel.result.toString()
     }
 }
